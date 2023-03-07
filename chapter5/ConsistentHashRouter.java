e51afc09204e158f0e56ba74340906bef87552ce
Himanshu Gaur <himanshugaur@Himanshus-MacBook-Air.local>
2023-03-07 20:27:11 +0530

    initializing hash router

--- chapter5/ConsistentHashRouter.java
+++ chapter5/ConsistentHashRouter.java
@@ -1,12 +1,29 @@
 package chapter5;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ConsistentHashRouter {
     private TreeMap<Integer,VirtualNode> hashRing=new TreeMap<>();
-
-    public ConsistentHashRouter(int physicalServers, int virtualNodesCount){
+    private List<PhysicalNode> physicalNodes=new ArrayList<>();
+    public ConsistentHashRouter(int physicalServers, int virtualNodesCount, int keyRangeUpperBound){
+        int totalvNodes=physicalServers*virtualNodesCount;
         
+        physicalNodes=IntStream.range(0, physicalServers)
+        .mapToObj(i->new PhysicalNode(i,virtualNodesCount))
+        .collect(Collectors.toList());
+
+        List<VirtualNode> virtualNodes=createVirtualServers(physicalNodes,virtualNodesCount);
+        assignVirtualNodesOnHashRing(virtualNodes,keyRangeUpperBound);
+    }
+    private void assignVirtualNodesOnHashRing(List<VirtualNode> virtualNodes, int keyRangeUpperBound) {
+    }
+    private List<VirtualNode> createVirtualServers(List<PhysicalNode> physicalNodes2, int virtualNodesCount) {
+        return null;
     }
 }
--- chapter5/PhysicalNode.java
+++ chapter5/PhysicalNode.java
@@ -3,5 +3,10 @@ package chapter5;
 public class PhysicalNode {
     private Integer nodeId;
     private Integer virtualNodes;
-    
+
+    public PhysicalNode(Integer nodeId, Integer virtualNodes) {
+        this.nodeId = nodeId;
+        this.virtualNodes = virtualNodes;
+    }
+
 }
