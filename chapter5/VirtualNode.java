27ec3bc685b6b25ae04a959dab34a5b44efdfebf
Himanshu Gaur <himanshugaur@Himanshus-MacBook-Air.local>
2023-03-07 20:57:52 +0530

    adding randomness to distirbute vNodes non-uniformally over hash ring

--- chapter5/VirtualNode.java
+++ chapter5/VirtualNode.java
@@ -1,5 +1,7 @@
 package chapter5;
 
+import java.util.Random;
+
 public class VirtualNode {
     private Integer id;
     private PhysicalNode physicalNode;
@@ -13,9 +15,13 @@ public class VirtualNode {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((physicalNode == null) ? 0 : physicalNode.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode()) * getSomeRandomInteger();
+        result = prime * result + ((physicalNode == null) ? 0 : physicalNode.hashCode()) * getSomeRandomInteger();
         return result;
+    }
+
+    private int getSomeRandomInteger() {
+        return new Random().nextInt();
     }
 
     @Override
@@ -40,6 +46,4 @@ public class VirtualNode {
         return true;
     }
 
-    
-    
 }
